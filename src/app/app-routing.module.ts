e32e3ce5be6c80064617803153880c65ef2352ba
giorgiocost <gegiorgio1991@gmail.com>
2020-09-28 23:47:13 -0300

    removido filmes de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FilmesComponent } from './pipes/filmes/filmes.component';
 import { ContadorComponent } from './components/contador/contator.component';
 
 const routes: Routes = [
-  { path: '', component: FilmesComponent },
   { path: 'contador', component: ContadorComponent }
 ];
 
